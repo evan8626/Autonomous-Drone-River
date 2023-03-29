423f4ce33b3ff434747405aa6eead09daf77ff2b
Evan Osborn <evan8626@gmail.com>
2023-03-29 11:03:29 -0700

    setup of tasks, adding a couple libraries, and setting of basic position for servo motor

--- Cleaning_Platform03122023.ino
+++ Cleaning_Platform03122023.ino
@@ -1,8 +1,9 @@
-#include <Adafruit_VL53L0X.h> // library for LiDar sensor
+#include <Adafruit_VL53L0X.h> // library for LiDar sensor must download Adafruit_VL53L0X latest version
 #include <Arduino_FreeRTOS.h> // library for FreeRTOS to run on arduino
 #include <SPI.h> // SPI communication protocol header
 #include <RF24.h> // Radio communication header must install RF24 by TMRh20 v 1.4.6 library for this to work
 #include <semphr.h> // semaphore header file
+#include <Servo.h> //servo header file
 #define rtDelay(v) {vTaskDelay(v/15);} // setting task delay time
 #define portCHAR char // defining portCHAR, used for FreeRTOS task setup
 
@@ -14,6 +15,11 @@ enum uint_8 {
   Motor75,    // Motor 75%
   Motor100    // Motor 100%
 };
+
+//function prototypes for tasks. The 'void *pvParameters' will change.
+void objDetect(void *pvParameters);
+void motion(void *pvParameters);
+void GPS(void *pvParameters);
 
 // Create an RF24 object
 RF24 radio(10, 11); // CE, CSN pins
@@ -40,11 +46,15 @@ const int echopin1 = 5;
 
 //UltraSonic sensor 2 (position: left)
 const int trigpin2 = 6;
-const int trigpin3 = 7;
+const int echopin2 = 7;
+
+void loop(){
+  //leave empty required for compilation of Arduino sketch
+}
 
 void setup() {
   // Initailization of communication hardware (subject to change, this is the RF24 
-  Serial.begin(9600); // setting serial communication rate to 9600 baud
+  Serial.begin(115200); // setting serial communication rate to 115200 meant for setup of serial comm between Arduino and RPi
   radio.begin(); //starts radio
   radio.setPALevel(RF24_PA_MIN); //Sets pwr amplification level, subject to change
   radio.setChannel(76); //Sets RF channel, subject to change
@@ -62,7 +72,7 @@ void setup() {
     Serial.println(F("failed to boot")); // if this lidar sensor is used this will become a syslog trace that is sent to the RPi
     while (1);
   }
-  Serial.println("lidar is online")); // if this lidar sensor is used this will become a syslog trace that is sent to the RPi
+  Serial.println("lidar is online"); // if this lidar sensor is used this will become a syslog trace that is sent to the RPi
 
   //Ultrasonic sensor setup
   pinMode(trigpin1, OUTPUT);
@@ -71,8 +81,56 @@ void setup() {
   pinMode(echopin2, INPUT);
   
   // Set up of tasks
-  // test comment
+  xTaskCreate(objDetect, "Objection Detection Task", 128, NULL, 1, NULL);
+  xTaskCreate(motion, "Motion Task", 128, NULL, 2, NULL);
+  xTaskCreate(GPS, "Location Task", 128, NULL, 3, NULL);
 
+  // Starting the scheduler
+  vTaskStartScheduler();
+}
+
+void objDetect(void *pvParameters){
+  (void) pvParameters;
+
+  for(;;){
+    //LiDAR sensors and ultrasonic sensors here
+    //...
+
+    //vTaskDelay to avoid deadlocks
+    vTaskDelay(pdMS_TO_TICKS(100)); //100 miliseconds
+  }
+}
+
+void motion(void *pvParameters){
+
+  // Function for when platform is in motion back to dock or to spot where it will collect
+  (void) pvParameters;
+
+  for(;;){
+    //Control of DC motors and servo motors here
+    //May need to add in call to Objdect? Must look into preemption 
+    //...
+    servo1.write(0); //writing a 0 to the motor to start in basic position
+
+    //vTaskDelay to avoid deadlocks
+    vTaskDelay(pdMS_TO_TICKS(100)); //100 miliseconds
+  }
+   
+}
+
+void GPS(void *pvParameters){
+
+  // Code for GPS location
+  (void) pvParameters;
+
+  for(;;){
+    //GPS signaling 
+    //...
+
+    //vTaskDelay to avoid deadlocks
+    vTaskDelay(pdMS_TO_TICKS(100)); //100 miliseconds
+  }
+  
 }
 
 void offLoad (){
@@ -81,17 +139,7 @@ void offLoad (){
   
 }
 
-void motion(){
 
-  // Function for when platform is in motion back to dock or to spot where it will collect
-   
-}
-
-void GPS(){
-
-  // Code for GPS location
-  
-}
 
 void garbage() {
   // where code for garbage collection will be sitting. Monitoring of sensors/fullness level. Also allows for some small amount of motor control to make sure platform stays put.
