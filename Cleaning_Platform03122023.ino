6a6d40f0521f3f70e6157369015e03469e26bd42
Evan Osborn <evan8626@gmail.com>
2023-04-05 20:30:44 -0700

    Added setup of ultrasonic sensors and servo control

--- Cleaning_Platform03122023.ino
+++ Cleaning_Platform03122023.ino
@@ -1,7 +1,7 @@
-#include <Adafruit_VL53L0X.h> // library for LiDar sensor must download Adafruit_VL53L0X latest version
+//#include <Adafruit_VL53L0X.h> // library for LiDar sensor must download Adafruit_VL53L0X latest version
 #include <Arduino_FreeRTOS.h> // library for FreeRTOS to run on arduino
 #include <SPI.h> // SPI communication protocol header
-#include <RF24.h> // Radio communication header must install RF24 by TMRh20 v 1.4.6 library for this to work
+//#include <RF24.h> // Radio communication header must install RF24 by TMRh20 v 1.4.6 library for this to work
 #include <semphr.h> // semaphore header file
 #include <Servo.h> //servo header file
 #define rtDelay(v) {vTaskDelay(v/15);} // setting task delay time
@@ -19,13 +19,14 @@ enum uint_8 {
 //function prototypes for tasks. The 'void *pvParameters' will change.
 void objDetect(void *pvParameters);
 void motion(void *pvParameters);
-void GPS(void *pvParameters);
+//void GPS(void *pvParameters);
 
+//No Arduino compatible radio transceiver yet
 // Create an RF24 object
-RF24 radio(10, 11); // CE, CSN pins
+//RF24 radio(10, 11); // CE, CSN pins
 
-const uint64_t pipe = 0xE8E8F0F0E1LL; // Address of the radio pipe
-const char text[] = "Hello, world!";
+//const uint64_t pipe = 0xE8E8F0F0E1LL; // Address of the radio pipe. This address will more than likely change
+//const char text[] = "Hello, world!";
 
 //DC Motor pins
 const int pwm = 2;  // DC motor speed control
@@ -38,7 +39,8 @@ Servo servo1; //creates servo1 object
 // may be adding a volatile variable here for interrupts of motion
 
 // LiDAR sensor may be subject to change as a hardwired LiDar would be better than a standalone package
-Adafruit_VL53L0X lidar = Adafruit_VL53L0X();
+// LiDAR not included yet as I don't have any Arduino compatible LiDAR sensors
+//Adafruit_VL53L0X lidar = Adafruit_VL53L0X();
 
 //UltraSonic sensor 1 (position: right)
 const int trigpin1 = 4;
@@ -48,6 +50,11 @@ const int echopin1 = 5;
 const int trigpin2 = 6;
 const int echopin2 = 7;
 
+//Angle range
+const int MIN_ANGLE = -180;
+const int MAX_ANGLE = 180;
+const int MAX_DIST = 400; //max distance an ultrasonic sensor can sense. Again, this will probably change because we will use longer range ultrasonic sensors.
+
 void loop(){
   //leave empty required for compilation of Arduino sketch
 }
@@ -55,11 +62,11 @@ void loop(){
 void setup() {
   // Initailization of communication hardware (subject to change, this is the RF24 
   Serial.begin(115200); // setting serial communication rate to 115200 meant for setup of serial comm between Arduino and RPi
-  radio.begin(); //starts radio
-  radio.setPALevel(RF24_PA_MIN); //Sets pwr amplification level, subject to change
-  radio.setChannel(76); //Sets RF channel, subject to change
-  radio.openReadingPipe(1, pipe); //Sets addres of radio pipe
-  radio.startListening(); //Sets radio to receive mode
+//  radio.begin(); //starts radio
+//  radio.setPALevel(RF24_PA_MIN); //Sets pwr amplification level, subject to change
+//  radio.setChannel(76); //Sets RF channel, subject to change
+//  radio.openReadingPipe(1, pipe); //Sets addres of radio pipe
+//  radio.startListening(); //Sets radio to receive mode
   
   // Initialization of all motors (DC, Servo)
   pinMode(dcMotor1, OUTPUT); //driving motor
@@ -67,12 +74,13 @@ void setup() {
   servo1.attach(servo1Pin); //rudder control
   
   // Initialization of object dectection hardware (1 LiDar sensor, and 2 ultrasonic senosrs)
-  //LiDar
-  if(!lidar.begin()){
-    Serial.println(F("failed to boot")); // if this lidar sensor is used this will become a syslog trace that is sent to the RPi
-    while (1);
-  }
-  Serial.println("lidar is online"); // if this lidar sensor is used this will become a syslog trace that is sent to the RPi
+  //LiDar 
+  // LiDAR not included yet as I don't have any Arduino compatible LiDAR sensors
+//  if(!lidar.begin()){
+//    Serial.println(F("failed to boot")); // if this lidar sensor is used this will become a syslog trace that is sent to the RPi
+//    while (1);
+//  }
+//  Serial.println("lidar is online"); // if this lidar sensor is used this will become a syslog trace that is sent to the RPi
 
   //Ultrasonic sensor setup
   pinMode(trigpin1, OUTPUT);
@@ -81,57 +89,94 @@ void setup() {
   pinMode(echopin2, INPUT);
   
   // Set up of tasks
-  xTaskCreate(objDetect, "Objection Detection Task", 128, NULL, 1, NULL);
+  //xTaskCreate(objDetect, "Objection Detection Task", 128, NULL, 1, NULL);
   xTaskCreate(motion, "Motion Task", 128, NULL, 2, NULL);
-  xTaskCreate(GPS, "Location Task", 128, NULL, 3, NULL);
+  //xTaskCreate(GPS, "Location Task", 128, NULL, 3, NULL);
 
   // Starting the scheduler
   vTaskStartScheduler();
 }
 
-void objDetect(void *pvParameters){
-  (void) pvParameters;
+//Ultrasonic sensor function to determine distance
+void ultraSonic_sensor(uint32_t *distance1, uint32_t *distance2){
+  long dur1 = 0, dur2 = 0; //timers to determine how long it takes a signal to travel from the ultrasonic sensor and back again
 
-  for(;;){
-    //LiDAR sensors and ultrasonic sensors here
-    //...
-
-    //vTaskDelay to avoid deadlocks
-    vTaskDelay(pdMS_TO_TICKS(100)); //100 miliseconds
-  }
+  //clearing trig pins of both ultrasonic sensors
+  digitalWrite(trigpin1, LOW);
+  digitalWrite(trigpin2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin1, HIGH);
+  digitalWrite(trigpin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin1, LOW);
+  digitalWrite(trigpin2, LOW);
+  dur1 = pulseIn(echopin1, HIGH);
+  dur2 = pulseIn(echopin2, HIGH);
+  //calculation of distance in cm (this will change later as we will get longer range ultrasonic sensors that can measure in meters)
+  *distance1 = (dur1 * 0.034)/2; // this equation is (time * speed of sound as cm/ms)/ round trip of signal (think of the distance there and the distance back. If we only multiply by 0.034 we're essentially doubling the distance even though the signal only traveled x feet)
+  *distance2 = (dur2 * 0.034)/2;
 }
+
+void control_servo(uint32_t distance1, uint32_t distance2){
+  int32_t dist_diff;
+  int32_t angle;
+  if(distance1 > distance2){ // So long as the difference between 1 and 2 favors 1, will write the angle to the servo depending on distance to ultrasonic sensor
+    dist_diff = (int32_t)distance1 - (int32_t)distance2;
+    angle = (dist_diff * (MAX_ANGLE - MIN_ANGLE))/((MAX_ANGLE - MIN_ANGLE)/2);
+    servo1.write(angle + 90);
+  }
+  else if(distance2 > distance1){ // So long as the difference between 1 and 2 favors 2, will write the angle to the servo depending on distance to ultrasonic sensor
+    dist_diff = (int32_t)distance2 - (int32_t)distance1;
+    angle = (dist_diff * (MAX_ANGLE - MIN_ANGLE))/((MAX_ANGLE - MIN_ANGLE)/2); 
+    servo1.write(angle + 90);
+  }
+  servo1.write(angle + 90);
+  ultraSonic_sensor(&distance1, &distance2);
+}
+
+//void objDetect(void *pvParameters){
+//  (void) pvParameters;
+//
+//  for(;;){
+//    //LiDAR sensors and ultrasonic sensors here
+//    //LiDAR not included yet as I don't have any Arduino compatible LiDAR sensors
+//    //...
+//    
+//    //vTaskDelay to avoid deadlocks
+//    vTaskDelay(pdMS_TO_TICKS(100)); //100 miliseconds
+//  }
+//}
 
 void motion(void *pvParameters){
 
   // Function for when platform is in motion back to dock or to spot where it will collect
-  (void) pvParameters;
+  (void) pvParameters;  // This is to avoid compiler warnings about unused parameters
 
-  for(;;){
-    //Control of DC motors and servo motors here
-    //May need to add in call to Objdect? Must look into preemption 
-    //...
-    servo1.write(0); //writing a 0 to the motor to start in basic position
+    while (1) {
+        uint32_t distance1, distance2;
+        uint32_t combined_distance;
+        ultraSonic_sensor(&distance1, &distance2);
+        control_servo(&distance1, &distance2);  // Control the servo motor based on the combined distance
 
-    //vTaskDelay to avoid deadlocks
-    vTaskDelay(pdMS_TO_TICKS(100)); //100 miliseconds
-  }
-   
+        vTaskDelay(pdMS_TO_TICKS(100));  // Reduce the delay for faster response, adjust as needed
+    }
 }
 
-void GPS(void *pvParameters){
-
-  // Code for GPS location
-  (void) pvParameters;
-
-  for(;;){
-    //GPS signaling 
-    //...
-
-    //vTaskDelay to avoid deadlocks
-    vTaskDelay(pdMS_TO_TICKS(100)); //100 miliseconds
-  }
-  
-}
+// Commenting this out for the time being as I have no GPS module to hardwire to the Ardiuno. Plus, this may work better on the RPi connection.
+//void GPS(void *pvParameters){
+//
+//  // Code for GPS location
+//  (void) pvParameters;
+//
+//  for(;;){
+//    //GPS signaling 
+//    //...
+//
+//    //vTaskDelay to avoid deadlocks
+//    vTaskDelay(pdMS_TO_TICKS(100)); //100 miliseconds
+//  }
+//  
+//}
 
 void offLoad (){
 
